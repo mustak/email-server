bf6edb10244328e52c7305d843c260caa95c5865
mustak <mhyaqub@yahoo.com>
2021-08-13 15:02:22 +0100

    add http status codes

--- src/controllers/cntlContacts.ts
+++ src/controllers/cntlContacts.ts
@@ -9,6 +9,7 @@ export const listContacts = async (inRequest: Request, inResponse: Response) => 
   try {
     const contactsWorker: Worker = new Worker();
     const contacts: IContact[] = await contactsWorker.listContacts();
+    inResponse.statusCode = 200;
     inResponse.json(contacts);
   } catch (inError) {
     inResponse.send('error');
@@ -19,7 +20,7 @@ export const addContact = async (inRequest: Request, inResponse: Response) => {
   try {
     const contactsWorker: Worker = new Worker();
     const contact: IContact = await contactsWorker.addContact(inRequest.body);
-    inResponse.json(contact);
+    inResponse.status(201).json(contact);
   } catch (inError) {
     inResponse.send('error');
   }
@@ -35,7 +36,7 @@ export const updateContact = async (inRequest: Request, inResponse: Response) =>
     };
 
     const contact: IContact = await contactsWorker.updateSingleContact(updateContact);
-    inResponse.json(contact);
+    inResponse.status(200).json(contact);
   } catch (inError) {
     inResponse.send('error');
   }
